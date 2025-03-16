a37dc92db72ab3e583731be59cd738438671c1e0
Rehan-252 <crehan252@gmail.com>
2025-03-16 19:58:54 +0530

    Add new file

---
+++ Pascalsarry.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Pascalsarry {
+
+    static void printarry(int arry[][]){
+        for(int i = 0; i < arry.length; i++){
+            for(int j = 0; j < arry[i].length;j++){
+                System.out.print(arry[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void pascal(int num){
+        int arry[][] = new int[num][];
+        for(int i = 0; i < num; i++){
+            arry[i] = new int[i+1];
+            arry[i][0] = arry[i][i] = 1;
+
+            for(int j = 1; j < i; j++){
+                arry[i][j] = arry[i-1][j] + arry[i-1][j-1];
+            }
+        }
+
+        printarry(arry);
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.println("ENTER YOUR NUMBER ");
+            int num = in.nextInt();
+            pascal(num);
+        }
+        
+    }
+}
